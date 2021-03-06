5a7db20cfc402f2be9af62f5795a3f0a708293c7
Dan Chrostowski <dan@danchrostowski.com>
2021-03-06 13:06:55 -0700

    change tournament

--- src/components/standings.js
+++ src/components/standings.js
@@ -70,7 +70,7 @@ function createTable(rows, tname, numPlayers, classes) {
 function getChefCatTournament(tourneyData) {
     console.log(tourneyData)
     try {
-        let re = new RegExp(/daily\scrust/,'i')
+        let re = new RegExp(/freeroll/,'i')
         const tournamentNames = Object.keys(tourneyData)
 
         for (let i=0; i<tournamentNames.length; i++) {
