7ef869ba51e4c64bd5606364f698d8598b1ce9b7
Dan Chrostowski <dan@danchrostowski.com>
2021-04-02 00:52:22 -0600

    change update interval to 45 seconds

--- src/components/standings.js
+++ src/components/standings.js
@@ -99,7 +99,7 @@ function Standings(props) {
             setInterval(async () => {
 
                 props.getTournamentData()
-            }, 20000)
+            }, 45000)
 
         }
         catch(e) {
